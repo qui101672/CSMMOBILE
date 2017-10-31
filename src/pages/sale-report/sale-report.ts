6350705c7286e0637266dab862fe0f0a5e1556c0
unknown <qui101672@gmail.com>
2017-10-31 16:36:33 +0700

    change sale-report

--- src/pages/sale-report/sale-report.ts
+++ src/pages/sale-report/sale-report.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 /**
  * Generated class for the SaleReportPage page.
  *
@@ -15,11 +16,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SaleReportPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  sales: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+    this.http.get('http://10.151.122.8:8080/api/getPaymentList').map(res => res.json()).subscribe(data => {
+      this.sales = data;
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SaleReportPage');
   }
-
+  
 }
